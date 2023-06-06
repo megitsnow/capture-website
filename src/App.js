868caa529db52795a6d61dc62a0d0251da1860f0
megitsnow <meganmarie.nieve@gmail.com>
2023-06-06 15:38:29 -0700

    styled the about us section and added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import AboutUs from './pages/AboutUs'
+import GlobalStyle from './components/GlobalStyle';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+import Nav from './components/Nav';
+import {Switch, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className = "App">
-      <AboutUs/>
+      <GlobalStyle/>
+      <Nav/>
+      
+      <Routes>
+        <Route path="/" element = {<AboutUs/>}/>
+        <Route path="/work" element = {<OurWork/>}/>
+        <Route path="/contact" element = {<ContactUs/>}/>
+      </Routes>
+      
+      
     </div>
   );
 }
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection'
+import ServicesSection from '../components/ServicesSection'
+import FAQSection from '../components/FAQSection'
 
 const AboutUs = () => {
     return (
         <div>
             <AboutSection/>
+            <ServicesSection/>
+            <FAQSection/>
         </div>
     )
 }
